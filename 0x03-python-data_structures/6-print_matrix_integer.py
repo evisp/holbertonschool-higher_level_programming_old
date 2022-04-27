0642094014ce159b762a48b95d7d3a7c893336b7
evisp <evisplaku@gmail.com>
2022-04-27 11:23:20 -0700

    Fix: style

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
-        print(" ".join("{}".format(element) for element in line))
+        print(" ".join("{:d}".format(element) for element in line))
