1660b490ac7377a5ef4264f15584169c043ea2a5
evisp <evisplaku@gmail.com>
2022-04-27 13:43:26 -0700

    Add: task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    bool_list = []
+    for value in my_list:
+        if value % 2 == 0:
+            bool_list.append(True)
+        else:
+            bool_list.append(False)
+    return bool_list
