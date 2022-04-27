087ffd6762eed620f3b2d32ed5b373bc48523311
evisp <evisplaku@gmail.com>
2022-04-27 05:18:40 -0700

    Fix: print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if nr_args == 0:
         print("0 arguments.")
     elif nr_args == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
         print("{} arguments:".format(nr_args))
 
