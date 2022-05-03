6824f84de949cab09726731ffcf2fbe29a7c38bc
evisp <evisplaku@gmail.com>
2022-05-03 04:09:33 -0700

    Add: task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    translate = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    translate = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+        "IV": 4,
+        "IX":9,
+        "XL":40,
+        "XC":90,
+        "CD":400,
+        "CM":900
+    }
     i = 0
     num = 0
     while i < len(roman_string):
