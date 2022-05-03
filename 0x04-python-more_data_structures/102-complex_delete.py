22af8e29c2c38fc2e3d7fa3568844780d14f9274
evisp <evisplaku@gmail.com>
2022-05-03 03:54:39 -0700

    Add: task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary,value):
+    return {k:v for k, v in a_dictionary.items() if v != value}
+    """
     for k, v in a_dictionary.items():
         if v == value:
             del a_dictionary[k]
             break
     return (a_dictionary)
+    """
+    
