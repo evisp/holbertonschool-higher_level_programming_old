3d29dac95663ce83a8c869aaf90c704bb83f6d52
evisp <evisplaku@gmail.com>
2022-05-04 21:12:44 -0700

    Fix: typo

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,33 +1,26 @@
 #include "lists.h"
-/**
- * is_palindrome - checks if a singly inked list is a palindrome
- * @head: a double pointer to the head of a linked list
- * Return: 0 if is not palindrome and 1 if its palindrome
- */
+
 int is_palindrome(listint_t **head)
 {
-	listint_t *rove_list = *head;
+	listint_t *reverse_list = *head;
 	int size, index, buffer[2048];
 
 	size = 0;
 	index = 0;
-	if (!(*head) || !rove_list->next)
+	if (*head == NULL || reverse_list->next == NULL)
 		return (1);
-	while (rove_list)
+	/* copy list to buffer array */ 
+	while (reverse_list)
 	{
-		/* save in buffer the size of content of he list */
-		buffer[size] = rove_list->n;
-		rove_list = rove_list->next;
+		buffer[size] = reverse_list->n;
+		reverse_list = reverse_list->next;
 		size++;
 	}
-	/* compare the list from the ends to the middle */
+	/* compare from the end to the middle of the list */
 	while (size >= index)
 	{
 		if (buffer[index] != buffer[size - 1])
 			return (0);
-		/* e.g index + 1 is node(1)*/
-		/* size = 11 (with null) -1 is position buffer at 10 */
-		/* which is node(1) */
 		size--;
 		index++;
 	}
