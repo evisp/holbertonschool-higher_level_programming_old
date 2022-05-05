8c53be60f68ef62fcebdfc6987f3ee02592f506a
evisp <evisplaku@gmail.com>
2022-05-04 21:15:56 -0700

    Add: check if linked list is palindrome

--- 0x03-python-data_structures/13-ispal.c
+++ 0x03-python-data_structures/13-ispal.c
@@ -2,17 +2,18 @@
 
 int is_palindrome(listint_t **head)
 {
+	listint_t *reverse_list = *head;
 	int size, index, buffer[2048];
 
 	size = 0;
 	index = 0;
-	if (*head == NULL || (*head)->next == NULL)
+	if (*head == NULL || reverse_list->next == NULL)
 		return (1);
 	/* copy list to buffer array */ 
-	while (*head)
+	while (reverse_list)
 	{
-	        buffer[size] = (*head)->n;
-		*head = (*head)->next;
+		buffer[size] = reverse_list->n;
+		reverse_list = reverse_list->next;
 		size++;
 	}
 	/* compare from the end to the middle of the list */
